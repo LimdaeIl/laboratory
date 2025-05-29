3096e6b8260880763ae6ce1617bd9259ab030272
LimdaeIl <optional94@naver.com>
2025-05-29 19:12:59 +0900

    #9: Spring Security Config
    스프링 시큐리티를 추가한 가장 큰 이유들 중 한 가지가 바로 이번 커밋 내용이다.
    
    CORS, CSRF, Http Basic 을 확실하게 비활성화 보안 요소를 직접 구현하는 것은 상당히 도전적이기 때문이다. 그리고 stateless를 보장해야만 했다.
    
    지금 requestMatchers()로 허용된 엔드포인트는 개발 마지막에 제대로 설정할 것 같다.

---
+++ src/main/java/com/book/laboratory/common/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.book.laboratory.common.config;
+
+import com.book.laboratory.common.filter.JwtTokenFilter;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+  private final JwtTokenFilter jwtTokenFilter;
+
+  @Bean
+  public PasswordEncoder makePasswordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .cors(cors -> cors.configurationSource(configurationSource()))
+        .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .sessionManagement(AbstractHttpConfigurer::disable);
+
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/users/**").permitAll()
+            .anyRequest().authenticated());
+
+    http
+        .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+    return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource configurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
+  }
+}
