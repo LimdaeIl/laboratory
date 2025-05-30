1c5e23e1e0cecb277f313caa0119386f50ad4b6c
LimdaeIl <optional94@naver.com>
2025-05-30 15:03:05 +0900

    #17: 로그인 서비스 설정
    ResponseCookie로 리프레시 토큰의 jti 전달
    
    더이상 Body 안에 토큰 X

--- src/main/java/com/book/laboratory/user/application/UserService.java
+++ src/main/java/com/book/laboratory/user/application/UserService.java
@@ -3,11 +3,12 @@ package com.book.laboratory.user.application;
 import com.book.laboratory.user.application.dto.request.LoginRequestDto;
 import com.book.laboratory.user.application.dto.request.SignupRequestDto;
 import com.book.laboratory.user.application.dto.response.LoginResponseDto;
+import com.book.laboratory.user.application.dto.response.LoginResponseWithCookieDto;
 import com.book.laboratory.user.application.dto.response.SignupResponseDto;
 import jakarta.validation.Valid;
 
 public interface UserService {
   SignupResponseDto signup(SignupRequestDto requestDto);
 
-  LoginResponseDto login(LoginRequestDto requestDto);
+  LoginResponseWithCookieDto login(LoginRequestDto requestDto);
 }
--- src/main/java/com/book/laboratory/user/application/UserServiceImpl.java
+++ src/main/java/com/book/laboratory/user/application/UserServiceImpl.java
@@ -2,14 +2,19 @@ package com.book.laboratory.user.application;
 
 import com.book.laboratory.common.exception.CustomException;
 import com.book.laboratory.common.jwt.JwtService;
+import com.book.laboratory.common.redis.RedisKeySupport;
+import com.book.laboratory.common.redis.RedisService;
 import com.book.laboratory.user.application.dto.request.LoginRequestDto;
 import com.book.laboratory.user.application.dto.request.SignupRequestDto;
 import com.book.laboratory.user.application.dto.response.LoginResponseDto;
+import com.book.laboratory.user.application.dto.response.LoginResponseWithCookieDto;
 import com.book.laboratory.user.application.dto.response.SignupResponseDto;
 import com.book.laboratory.user.domain.User;
 import com.book.laboratory.user.domain.UserErrorCode;
 import com.book.laboratory.user.domain.UserRepository;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +26,7 @@ public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
   private final JwtService jwtService;
+  private final RedisService redisService;
 
   private void existsUserByEmail(String email) {
     if (userRepository.existsUserByEmail(email)) {
@@ -52,7 +58,7 @@ public class UserServiceImpl implements UserService {
 
   @Transactional
   @Override
-  public LoginResponseDto login(LoginRequestDto requestDto) {
+  public LoginResponseWithCookieDto login(LoginRequestDto requestDto) {
     User userByEmail = findUserByEmail(requestDto.email());
 
     if (!passwordEncoder.matches(requestDto.password(), userByEmail.getPassword())) {
@@ -62,7 +68,25 @@ public class UserServiceImpl implements UserService {
     String accessToken = jwtService.generateAccessToken(userByEmail);
     String refreshToken = jwtService.generateRefreshToken(userByEmail);
 
-    return LoginResponseDto.from(userByEmail, accessToken, refreshToken);
+    String jtiByRT = jwtService.getTokenId(refreshToken);
+    Duration ttlByRT = Duration.ofMillis(jwtService.getRefreshTokenTtl());
+    String keyByRT = RedisKeySupport.refreshToken(jtiByRT);
+
+    redisService.set(keyByRT, refreshToken, ttlByRT);
+
+    ResponseCookie refreshCookie = ResponseCookie.from("RT", jtiByRT)
+        .httpOnly(true)
+        .secure(true)
+        .sameSite("Strict")
+        .path("/")
+        .maxAge(Duration.ofMillis(jwtService.getRefreshTokenTtl()))
+        .build();
+
+    return new LoginResponseWithCookieDto(
+        LoginResponseDto.from(userByEmail),
+        refreshCookie,
+        accessToken
+    );
   }
 
 
