c7a30bc012c0d946fa3df9be10b605c22d6b5908
LimdaeIl <optional94@naver.com>
2025-05-30 12:37:32 +0900

    #15: 마이페이지 조회 테스트 코드 파일명 수정
    직관적!

--- src/test/java/com/book/laboratory/user/application/UserServiceGetMyInfoTest.java
+++ src/test/java/com/book/laboratory/user/application/UserServiceGetMyInfoTest.java
@@ -21,7 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class UserServiceImplTest {
+class UserServiceGetMyInfoTest {
 
   @InjectMocks
   private UserServiceImpl userService;
