ba036a14c8ebb720e362d39e9fd5351bf304ea00
LimdaeIl <optional94@naver.com>
2025-06-01 10:49:49 +0900

    #22: 검색 조건 Condition 생성
    검색 조건은 Condition으로 DTO와 동일한 역할을 수행하는 것으로 봐도 된다.

---
+++ src/main/java/com/book/laboratory/user/application/dto/condition/UserSearchCondition.java
@@ -0,0 +1,39 @@
+package com.book.laboratory.user.application.dto.condition;
+
+import com.book.laboratory.user.domain.SocialType;
+import com.book.laboratory.user.domain.UserRole;
+import java.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public record UserSearchCondition(
+    Long id,
+    String name,
+    String email,
+    UserRole userRole,
+    SocialType socialType,
+    String socialId,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime createdFrom,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime createdTo,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime updatedFrom,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime updatedTo,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime deletedFrom,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime deletedTo,
+
+    Long createdBy,
+    Long updatedBy,
+    Long deletedBy
+
+) {
+}
